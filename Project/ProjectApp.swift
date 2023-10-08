45d3a63289e1927f06537516da806f66bf2ae221
a-mystic <totocaca@naver.com>
2023-10-08 17:07:55 +0900

    first commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by a mystic on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
